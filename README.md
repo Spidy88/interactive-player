b23e01f0e152429bf96ffa23720c6fac6d5d64e8
Jonathan Mason <jmason.p@gmail.com>
2015-12-31 13:05:50 -0600

    Alters time system to rely on seconds rather than video percentage. Update README and comments.
    Rewrites existing time tracking functions to use seconds instead of video percent.
    
    Removes unused styles.
    
    Corrects comments to refer to updated functions and variables.
    
    Update README with information about the component.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# interactive-player
-A web component that creates a video player with location based comment history. 
+# <interactive-player>
+A Vimeo video player with comments and video progress based comment highlighting. Comments show/hide using the off-canvas technique.
+
+The player accepts several attributes, one of which is required: 
+
+`source` - the URL of the Vimeo video to be played. 
+
+Optional: 
+
+`comments`  - (Array) of Objects containing the video comments and the following data about them: 
+  `author` - (String) name of the user who wrote the comment 
+  `text` - (String) message text 
+  `createdAt` - (Number) moment of comment creation (in seconds) 
+  `timestamp` - (String) a human readable conversion of the createdAt time
+
+`highlight` - (Boolean) determines whether or not comment highlighting is active. Default to true. 
+
+
+User written comments are made available through the `new-comment` event which provides an object with the following properties:
+`text` - (String)  
+`createdAt` - (Number)
+`timestamp` - (String)
+
+All properties are the same as above. 
+
+Demo viewable in index.html.
--- testComments.js
+++ testComments.js
@@ -1,45 +1,54 @@
 var testComments = [{
           author: 'Timmy',
-          text: 'Bacon ipsum dolor amet pancetta beef drumstick ut aute ball tip velit, dolore cillum lorem picanha culpa strip steak pastrami shoulder. Commodo capicola biltong, jerky anim aliquip qui pariatur. Biltong t-bone in, bacon adipisicing duis prosciutto drumstick. Minim biltong rump tenderloin ex ullamco in brisket non ut. Brisket anim esse elit. Ut officia incididunt shoulder andouille occaecat.', 
-          createdAt: 0.002
+          text: 'Bacon ipsum dolor amet pancetta beef drumstick ut aute ball tip velit, dolore cillum lorem picanha culpa strip steak pastrami shoulder. Commodo capicola biltong, jerky anim aliquip qui pariatur. Biltong t-bone in, bacon adipisicing duis prosciutto drumstick. Minim biltong rump tenderloin ex ullamco in brisket non ut. Brisket anim esse elit. Ut officia incididunt shoulder andouille occaecat.',
+          timestamp: "00:10",
+          createdAt: 10
         },
         {
           author: 'Barbara',
           text: 'Porchetta ut elit do. Ham hock ball tip in cupidatat ipsum pig consectetur beef. Pig fugiat duis prosciutto, eu boudin dolor velit. Frankfurter hamburger chuck drumstick aliquip venison ut pork loin ham ground round pancetta porchetta.', 
-          createdAt: 0.999
+          timestamp: "21:30",
+          createdAt: 1290
         },
         {
           author: 'Susan',
           text: 'Dolore turducken venison dolore do frankfurter flank nostrud irure. Nisi labore brisket nulla. Pancetta cupidatat proident eiusmod. Filet mignon in nisi, pig excepteur pork belly velit ea id kielbasa ball tip.', 
-          createdAt: 0.7
+          timestamp: "26:17",
+          createdAt: 1577
         },
         {
           author: 'Susan',
           text: 'Dolore turducken venison dolore do frankfurter flank nostrud irure. Nisi labore brisket nulla. Pancetta cupidatat proident eiusmod. Filet mignon in nisi, pig excepteur pork belly velit ea id kielbasa ball tip.', 
-          createdAt: 0.5
+          timestamp: "26:17",
+          createdAt: 1577
+        },
+        {
+          author: 'Barbara',
+          text: 'Porchetta ut elit do. Ham hock ball tip in cupidatat ipsum pig consectetur beef. Pig fugiat duis prosciutto, eu boudin dolor velit. Frankfurter hamburger chuck drumstick aliquip venison ut pork loin ham ground round pancetta porchetta.', 
+          timestamp: "28:42",
+          createdAt: 1722
         },
         {
           author: 'Susan',
           text: 'Dolore turducken venison dolore do frankfurter flank nostrud irure. Nisi labore brisket nulla. Pancetta cupidatat proident eiusmod. Filet mignon in nisi, pig excepteur pork belly velit ea id kielbasa ball tip.', 
-          createdAt: 0.5
+          timestamp: "35:49",
+          createdAt: 2149
         },
         {
           author: 'Barbara',
           text: 'Porchetta ut elit do. Ham hock ball tip in cupidatat ipsum pig consectetur beef. Pig fugiat duis prosciutto, eu boudin dolor velit. Frankfurter hamburger chuck drumstick aliquip venison ut pork loin ham ground round pancetta porchetta.', 
-          createdAt: 0.888
+          timestamp: "37:05",
+          createdAt: 2225
         },
         {
           author: 'Barbara',
           text: 'Porchetta ut elit do. Ham hock ball tip in cupidatat ipsum pig consectetur beef. Pig fugiat duis prosciutto, eu boudin dolor velit. Frankfurter hamburger chuck drumstick aliquip venison ut pork loin ham ground round pancetta porchetta.', 
-          createdAt: 0.4
+          timestamp: "46:48",
+          createdAt: 2808
         },
         {
           author: 'Barbara',
           text: 'Porchetta ut elit do. Ham hock ball tip in cupidatat ipsum pig consectetur beef. Pig fugiat duis prosciutto, eu boudin dolor velit. Frankfurter hamburger chuck drumstick aliquip venison ut pork loin ham ground round pancetta porchetta.', 
-          createdAt: 0.543
-        },
-        {
-          author: 'Barbara',
-          text: 'Porchetta ut elit do. Ham hock ball tip in cupidatat ipsum pig consectetur beef. Pig fugiat duis prosciutto, eu boudin dolor velit. Frankfurter hamburger chuck drumstick aliquip venison ut pork loin ham ground round pancetta porchetta.', 
-          createdAt: 0.678
+          timestamp: "52:29",
+          createdAt: 3149
         }];
